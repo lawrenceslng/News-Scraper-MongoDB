d865c11926e6a6a732f616e3b6468824451df591
LSLNG <lawrencesuilunng@gmail.com>
2018-09-23 22:55:56 -0700

    comment id on front end

--- public/app.js
+++ public/app.js
@@ -15,14 +15,17 @@ function getResults() {
             for (var i = 0; i < data.length; i++) {
                 var newDiv = $("<div>").attr("data-id", data[i]._id);
                 var saveButton = $("<button class='save'>").text("Save this Article");
+                var comments = $("<span>"+data[i].comments+"</span>");
+                var commentForm = $("<form>");
                 var commentButton = $("<button type='button' class='btn btn-primary comment'>").text("View/Add a Comment");
                 var commentTextBox = $("<textarea class='d-none' rows='2' placeholder='Put your Comment Here!'>");
                 var commentSubmitButton = $("<button class='submit d-none btn btn-primary'>").text("Add a Comment");
-                newDiv.html("<h5>"+data[i].title+"</h5> <span>"+data[i].summary+"</span> \n <a href = " + data[i].link+">Article Link</a> \n");
+                newDiv.html("<h5>"+data[i].title+"</h5> <span>"+data[i].summary+"</span> <br> <a href = " + data[i].link+">Article Link</a>");
                 newDiv.append(saveButton);
                 newDiv.append(commentButton);
-                newDiv.append(commentSubmitButton);
-                newDiv.append(commentTextBox);
+                commentForm.append(commentTextBox);
+                commentForm.append(commentSubmitButton);
+                newDiv.append(commentForm);
                 $(".results").prepend(newDiv);
             }
         }  
@@ -59,23 +62,47 @@ $(document).on("click",".clear",function(){
 })
 
 //this function will pop a modal box so that users can leave a comment and their name for a particular article
-$(document).on("click", ".submit", function() {
+$(document).on("click", ".submit", function(e) {
+    e.preventDefault();
     console.log("comment saving");
-    $(this).parent()
+    var element = $(this).parents("div");
+    element = $(element).attr("data-id");
+    console.log(element);
+
+
+    // $.ajax({
+    //     type: "POST",
+    //     dataType: "json",
+    //     url: "/comment",
+    //     data: {
+    //       title: $("#title").val(),
+    //       note: $("#note").val(),
+    //       created: Date.now()
+    //     }
+    //   })
+    //   // If that API call succeeds, add the title and a delete button for the note to the page
+    //     .then(function(data) {
+    //     // Add the title and delete button to the #results section
+    //       $("#results").prepend("<p class='data-entry' data-id=" + data._id + "><span class='dataTitle' data-id=" +
+    //       data._id + ">" + data.title + "</span><span class=delete>X</span></p>");
+    //       // Clear the note and title inputs on the page
+    //       $("#note").val("");
+    //       $("#title").val("");
+    //     });
     // $("#commentForm").attr("method","POST");
     // $("#commentForm").attr("action","/comment");
     // $("#commentForm").submit();
 });
 
 $(document).on("click",".comment",function(event){
-    if($(this).parent().children("textarea").hasClass("d-none"))
+    if($(this).parent().children("form").children("textarea").hasClass("d-none"))
     {
-        $(this).parent().children("textarea").removeClass("d-none");
-        $(this).parent().children(".submit").removeClass("d-none");
+        $(this).parent().children("form").children("textarea").removeClass("d-none");
+        $(this).parent().children("form").children(".submit").removeClass("d-none");
     }
     else{
-        $(this).parent().children("textarea").addClass("d-none");
-        $(this).parent().children(".submit").addClass("d-none");
+        $(this).parent().children("form").children("textarea").addClass("d-none");
+        $(this).parent().children("form").children(".submit").addClass("d-none");
     }
     
 })
